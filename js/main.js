2092014c954be7d43c7b49deeb943f43418436bc
Anas Alnagar <35266228+onisEg@users.noreply.github.com>
2022-12-15 00:48:49 +0200

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,61 @@
+
+
+let countriesList = [];
+fetch("https://restcountries.com/v3.1/all").then((response) => {
+    return response.json()
+}).then((date) => {
+    countriesList = date;
+    showCountries(countriesList)
+}).catch(error => { throw error }).finally(() => { })
+
+
+let showCountries = country => {
+    // console.table(country);
+    let tbody = document.getElementById("tbody");
+    tbody.innerHTML = "";
+    let countriesListRow = "";
+    country.forEach(country => {
+        countriesListRow += `
+            <tr>
+                <td>${country.name.official}</td>
+                <td>${country.independent ? 'yes' : 'No'}</td>
+                <td>${country.capital}</td>
+                <td>${country.region}</td>
+            </tr>
+        `
+    });
+    tbody.innerHTML = countriesListRow;
+}
+
+
+// filter by countery name 
+const nameField = document.getElementById('filterName');
+nameField.addEventListener("keydown", event => {
+    let searchStr = event.target.value.toLowerCase();
+    // console.log(searchStr);
+    if (searchStr) {
+        let countries = countriesList.slice();
+        let filteredCountries = countries.filter(country => country.name.official.toLowerCase().includes(searchStr));
+        showCountries(filteredCountries)
+
+    } else {
+        showCountries(countriesList);
+    }
+})
+
+
+// filter by region name 
+const regionField = document.getElementById('filterRegion');
+regionField.addEventListener("keydown", event => {
+    let searchStr = event.target.value.toLowerCase();
+    // console.log(searchStr);
+    if (searchStr) {
+        let countries = countriesList.slice();
+        let filteredCountries = countries.filter(country => country.region.toLowerCase().includes(searchStr));
+        showCountries(filteredCountries)
+
+    } else {
+        showCountries(countriesList);
+    }
+})
+
